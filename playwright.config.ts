df6bb892546c9b75c895749e68677dd7d6c2cd8e
GrahamQuan <grahamquandev@gmail.com>
2026-05-04 21:12:14 +0800

    fix: linter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./apps/api/e2e",
+  testDir: './apps/api/e2e',
   timeout: 30_000,
 });
