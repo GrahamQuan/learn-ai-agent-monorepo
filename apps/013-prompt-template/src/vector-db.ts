32da1ec705cabd6df9f185d45bc10c25821db45f
GrahamQuan <grahamquan24@gmail.com>
2026-05-24 18:16:06 +0800

    feat: prompt-template

--- apps/013-prompt-template/src/vector-db.ts
+++ apps/013-prompt-template/src/vector-db.ts
@@ -1,7 +1,7 @@
 import { MilvusClient } from '@zilliz/milvus2-sdk-node';
 
-export const COLLECTION_NAME = 'weekly_report_examples';
-export const MILVUS_ADDRESS = 'localhost:19530';
+export const COLLECTION_NAME: string = 'conversations';
+export const MILVUS_ADDRESS: string = 'localhost:19530';
 
 export const vectorDatabaseClient = new MilvusClient({
   address: MILVUS_ADDRESS,
